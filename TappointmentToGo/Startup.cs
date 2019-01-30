24baf1c8ba50976420fda3b98b987c3d8570cdbb
David Leblay <leblaydavid7@gmail.com>
2019-01-30 09:20:12 +0100

    Add project files.

---
+++ TappointmentToGo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TappointmentToGo.Startup))]
+namespace TappointmentToGo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
