b757f049ab78c679854853660a2e7697d52c7b51
David Leblay <leblaydavid7@gmail.com>
2019-01-31 14:57:27 +0100

    Replaced item dictionary in Cart with a separate Table in DB

--- TappointmentToGo/Models/ApplicationDbContext.cs
+++ TappointmentToGo/Models/ApplicationDbContext.cs
@@ -13,5 +13,6 @@ namespace TappointmentToGo.Models
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
     }
 }
--- TappointmentToGo/Models/Cart.cs
+++ TappointmentToGo/Models/Cart.cs
@@ -8,6 +8,28 @@ namespace TappointmentToGo.Models
     public class Cart : AbstractItem
     {
         public string UserId { get; set; }
-        public Dictionary<MenuItem, int> ItemsDictionary { get; set; }
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+        public void Add(MenuItem menuItem) => Add(menuItem, 1);
+
+        public void Add(MenuItem menuItem, int count)
+        {
+            var x = CartItems.Find(item => item.MenuItem == menuItem);
+            if (x == null)
+                CartItems.Add(new CartItem(menuItem, count));
+            else
+                x.Count += count;
+        }
+    }
+
+    public class CartItem : AbstractItem
+    {
+        public MenuItem MenuItem { get; set; }
+        public int Count { get; set; }
+        public CartItem(MenuItem menuItem, int count)
+        {
+            MenuItem = menuItem;
+            Count = count;
+        }
     }
 }
