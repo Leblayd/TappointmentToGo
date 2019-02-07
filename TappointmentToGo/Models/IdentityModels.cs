1a7bda4f1ae25ac83177ef165a23480aa99ebc2e
David Leblay <leblaydavid7@gmail.com>
2019-02-07 16:54:05 +0100

    Fixed problem with Cart max amount set to 0

--- TappointmentToGo/Models/IdentityModels.cs
+++ TappointmentToGo/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,7 +10,17 @@ namespace TappointmentToGo.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public Cart Cart { get; set; } = new Cart(20000);
+        public static int CartMaxAmount { get; set; } = 20000;
+        public Cart Cart { get; set; }
+        public ApplicationUser()
+        {
+            NewEmptyCart();
+        }
+        public void NewEmptyCart()
+        {
+            Cart = new Cart(CartMaxAmount);
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
