5a2f71c7e369201e21fae05932e7c94c8ddbee68
David Leblay <leblaydavid7@gmail.com>
2019-02-10 20:11:44 +0100

    Pluralized category names using Humanizer package + tiny fixes

--- TappointmentToGo/Models/Category.cs
+++ TappointmentToGo/Models/Category.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.Pluralization;
 using System.Linq;
 using System.Web;
+using Humanizer;
 
 namespace TappointmentToGo.Models
 {
@@ -9,6 +11,7 @@ namespace TappointmentToGo.Models
     {
         public string Name { get; set; }
         public List<MenuItem> MenuItems { get; set; }
+        public string NamePlural { get => Name.Pluralize(); }
         public string ImageName { get => Name.Replace(" ", string.Empty); }
     }
 }
